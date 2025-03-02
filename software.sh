2b0ac2107219596f770948626195f117fd1a65f1
Andrei Ladyka <andrei@ladyka.by>
2025-03-02 18:32:19 +0300

    Init

---
+++ software.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+#Monitoring
+sudo apt install htop -y
+sudo apt install btop -y
+
+
+#Development tools
+sudo apt install git -y
+sudo apt install docker.io -y
+sudo apt install openjdk-21-jdk -y
+
